84b2810fe471cb301273acec1941f1d5851aaf4b
Will <Will@DESKTOP-8EKFRLK>
2023-08-11 09:15:05 +1000

    for external

--- BatchRunner.m
+++ BatchRunner.m
@@ -3,14 +3,15 @@ function batch_results = BatchRunner(start_index,batch_size)
 batch_results = zeros(batch_size,1);
 
 % Get image files name from specified folder
-image_folder = 'images';
+image_folder = 'F:\images';
 names = dir(image_folder);
 names = names(3:end);
 
 % As seen as FileLoadExample
 for i = 1:batch_size
     names(start_index+(i-1)).name
-    fileloc = strcat(pwd,'\',image_folder,'\',names(start_index+(i-1)).name);
+    fileloc = strcat(image_folder,'\',names(start_index+(i-1)).name);
+    %fileloc = strcat(pwd,'\',image_folder,'\',names(start_index+(i-1)).name);
     DICOMnum = [];                      %Set DICOMnum to null if not a DICOM stack
     INFO.PixelSpacing = [0.51; 0.51] ;  %[0.51; 0.51] MIDAS; [0.47; 0.47] IMPERIAL
     INFO.SliceThickness = 0.8;          %0.8 MIDAS; 0.8 IMPERIAL
