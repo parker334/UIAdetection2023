a251775f8fdc7ef7d8e05e819b59412a95408fbd
Will <Will@DESKTOP-8EKFRLK>
2023-10-20 07:36:00 +1000

    Final Changes to BatchRunner

--- X_BatchRunner.m
+++ X_BatchRunner.m
@@ -1,4 +1,6 @@
 function [names_list, batch_results, batch_num_results, batch_info_results, rec_times, data_subpl_list, fitresult_list, outlierProps_list, KwPredictor_list, MaskVol_list, PathLength_list] = X_BatchRunner(start_index,batch_size)
+% Example
+% [names,is_pos,num,info,times,subpl,fitresult,outlier,kwpred,maskvol,pathlen]
 % Initialize results
 names_list = strings(batch_size,1);
 batch_results = zeros(batch_size,1);
@@ -14,14 +16,15 @@ MaskVol_list = zeros(batch_size,1);
 PathLength_list = zeros(batch_size,1);
 
 % Get image files name from specified folder
-image_folder = 'D:\images';
+image_folder = 'F:\healthy';
 names = dir(image_folder);
 names = names(3:end);
 
 % As seen as FileLoadExample
 for i = 1:batch_size
     is_error = 0;
-    names_list(i) = names(start_index+(i-1)).name
+    names(start_index+(i-1)).name
+    names_list(i) = names(start_index+(i-1)).name;
     fileloc = strcat(image_folder,'\',names(start_index+(i-1)).name);
     %fileloc = strcat(pwd,'\',image_folder,'\',names(start_index+(i-1)).name);
     DICOMnum = [];                      %Set DICOMnum to null if not a DICOM stack
@@ -31,7 +34,7 @@ for i = 1:batch_size
     INFO.StudyDescription = '';         %MIDAS Healthy; Imperial Healthy
     INFO.Modality = 'MR';               %MR; DSA; CTA
     
-    kT = 0.3134*mean(INFO.PixelSpacing')^-1.522;
+    kT = 0.6709*mean(INFO.PixelSpacing')^-0.839;
         %We've assessed 3 kT weight parameters in our paper:
     %1.78 for constant relationship
     %0.6709*mean(INFO.PixelSpacing')^-0.839 for 1.20 intercept at 0.51
@@ -41,16 +44,17 @@ for i = 1:batch_size
     try
         [anom_list, data_subpl,fitresult,time2run,outlierProps,KwPredictor,MaskVol,PathLength] = X_PipelineAlgorithm(fileloc,DICOMnum,INFO,names_list(i),kT);
         rec_times(i) = time2run;
+        outlierProps_list(i) = outlierProps;
     catch
         is_error = 1;
     end
     if is_error == 1
+        batch_results(i) = -1;
         batch_num_results(i) = -1;
         continue
     end
     data_subpl_list(i) = {data_subpl};
     fitresult_list(i) = {fitresult};
-    outlierProps_list(i) = outlierProps;
     KwPredictor_list(i) = KwPredictor;
     MaskVol_list(i) = MaskVol;
     PathLength_list(i) = PathLength;
